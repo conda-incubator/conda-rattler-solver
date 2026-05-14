b6fbf7c8c1b2e1e2c02e35c631fa0a4bb651e779
Jannis Leidel <jannis@leidel.info>
2026-05-14 07:43:14 -0700

    Use --solver=rattler CLI flag instead of platform-specific test scripts

--- recipe/run_test.bat
+++
@@ -1,3 +0,0 @@
-set CONDA_SOLVER=rattler
-conda create --dry-run scipy
-if errorlevel 1 exit /b 1
--- recipe/run_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-CONDA_SOLVER=rattler conda create --dry-run scipy
